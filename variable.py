fadeca31fbca312c9a83342e7a20c4830d72e895
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-19 04:01:16 +0600

    added variable

---
+++ variable.py
@@ -0,0 +1,24 @@
+# print() # This is a built in function in python.
+
+print("I am a Student")
+print('I am a Student')
+# This two syntex is right in python program because double quetation and single quetation support in python.
+
+# python addition.
+print(3+2)  # no variable diclare.
+# Output is: 5
+
+print(3+2) 
+print((3+2)+5)
+# Output is: 5, 10 
+
+# Declare variable.
+number = 3+2     # number is variable.
+print(number)  # variable print.
+# Output is: 5
+
+# Many variable uses
+number = 3+2 
+new_number = number+5
+print(new_number)
+# Output is: 10
