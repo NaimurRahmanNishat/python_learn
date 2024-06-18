f91173b36dca9750b8800e9eb315b2bdf2bd787d
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-19 04:37:10 +0600

    added user input

---
+++ 04_user_input.py
@@ -0,0 +1,21 @@
+print("What is your name: ")
+name = input()
+print("Hello" +name+ "congratulations")
+# Output is: HelloNishatcongratulations
+# Using format separate word correct.
+
+print("What is your name: ")
+name = input()
+print(f"Hello {name} congratulations") # f means formate.
+# Output is: Hello Nishat congratulations
+
+# Another system is.....
+print("What is your name: ")
+name = input()
+print("Hello "+name+" congratulations")
+# Output is: Hello Nishat congratulations
+
+# Shortest user input given system
+name = input("What is your name: ")
+print("Hello "+name+" congratulations")
+# Output is: Hello Nishat congratulations
