ccb025ac29519c9c8f793b9f68c7cb29378b1e0e
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-06-19 18:06:44 +0600

    added math

---
+++ 06_math.py
@@ -0,0 +1,53 @@
+# squre mathod
+number = 3 ** 2  # (3^2) in this formula
+print(number)
+# Output is : 9
+
+# Increment.
+x = 5
+x += 5  # x = x + 5
+print(x)
+# Output is: 10
+
+# Decrement.
+x = 5
+x -= 5    # x = x - 5
+print(x) 
+# Output is: 0
+
+number = 10 + 3 - 4 * 5 ** 4   # working system: (), **, *, /, +, -
+print(number)
+# Output is: -2487
+
+number = 10 + 3 - 4 * 5 ** 4   # working system: (), **, *, /, +, -
+print(abs(number))   # use abs (absolute) function output is not negative.
+# Output is: 2487
+
+number = 19/3
+print(number)
+# Output is : 6.33 This is fruction number.
+
+number = 19/3
+print(round(number)) # round is build in function.
+# Output is: 6    This is round value.
+
+
+import math    # math is a built in libarary in python.
+value = 25
+result = math.sqrt(value)  # sqrt means squre root. using math libarary.
+print(result)
+# Output is: 5.0
+
+import math    # math is a built in libarary in python.
+value = 26
+result = math.sqrt(value)  # sqrt means squre root. using math libarary.
+print(math.ceil(result))  # ceil is built in function.
+# Output is: 6   because ceil means ceiling (ceiling update main value not update point value)
+
+import math    # math is a built in libarary in python.
+value = 26
+result = math.sqrt(value)  # sqrt means squre root. using math libarary.
+print(math.floor(result))  # floor is built in function.
+# Output is: 5   because floor (floor not update main value. floor missing point value.So, answer is 5)
+
+# follow (math — Mathematical functions (https://docs.python.org/3/library/math.html)) many built in fuction in this website.
